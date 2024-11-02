ec8888e312a28a2bcc25baef3e8d18c4c1b40d26
LX-y-ka <alexandravirtualife@gmail.com>
2024-11-02 09:25:04 +0200

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,28 @@
  */
 function makeCalculator() {
   // write code here
+  const calculator = {
+    result: 0,
+  };
+
+  calculator.reset = () => {
+    calculator.result = 0;
+
+    return calculator;
+  };
+
+  calculator.add = (number) => (calculator.result += number);
+  calculator.subtract = (number) => (calculator.result -= number);
+  calculator.multiply = (number) => (calculator.result *= number);
+  calculator.divide = (number) => (calculator.result /= number);
+
+  calculator.operate = (callback, number) => {
+    callback(number);
+
+    return calculator;
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
