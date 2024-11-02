19b5716f30a39c7cbd1012c496eccfc73cba2d8f
LX-y-ka <alexandravirtualife@gmail.com>
2024-11-02 11:14:24 +0200

    add changes

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -7,7 +7,6 @@ function makeCalculator() {
   // write code here
   const calculator = {
     result: 0,
-    methods: [],
   };
 
   calculator.reset = () => {
@@ -21,20 +20,19 @@ function makeCalculator() {
   calculator.multiply = (number) => (calculator.result *= number);
 
   calculator.divide = (number) => {
-    if (number !== 0) {
-      calculator.result /= number;
+    if (number === 0) {
+      return 'number shouldn`t be 0';
     }
+
+    calculator.result /= number;
   };
 
-  calculator.methods.push(calculator.add);
-  calculator.methods.push(calculator.subtract);
-  calculator.methods.push(calculator.multiply);
-  calculator.methods.push(calculator.divide);
-
   calculator.operate = (callback, number) => {
-    if (calculator.methods.includes(callback)) {
-      callback(number);
+    if ((!callback) instanceof Function) {
+      return 'callback shoul be a function';
     }
+
+    callback(number);
 
     return calculator;
   };
